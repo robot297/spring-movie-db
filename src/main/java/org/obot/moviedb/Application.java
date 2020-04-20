8858748b07f3db4de27e7849960a1c2301608f33
Daniel Obot <danobot@hotmail.com>
2020-04-20 17:46:38 -0500

    Removed redundancies in code.

--- src/main/java/org/obot/moviedb/Application.java
+++ src/main/java/org/obot/moviedb/Application.java
@@ -4,10 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class Application {
 	private static Logger LOG = LoggerFactory.getLogger(Application.class);
 
